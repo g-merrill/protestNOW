96d3dec9ce0bfaf77d6508382f0fb4dc66d47ff8
Greg <gmerrill03@gmail.com>
2019-10-07 17:56:58 -0700

    Add switch and routes to profilepage

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -7,28 +7,38 @@ class ProfilePage extends Component {
 
   render() {
     return (
-      <div className='ProfilePage'>
-        <div className="data-ctnr">
-          <label className="data-left" htmlFor="username">Username:</label>
-          <p id="username" className="data-right">{this.props.user.username}</p>
-        </div>
-        <div className="data-ctnr">
-          <label className="data-left" htmlFor="firstName">First Name (optional):</label>
-          <p id="firstName" className="data-right">{this.props.user.firstName}</p>
-        </div>
-        <div className="data-ctnr">
-          <label className="data-left" htmlFor="lastInitial">Last Initial (optional):</label>
-          <p id="lastInitial" className="data-right">{this.props.user.lastInitial}</p>
-        </div>
-        <div className="col-sm-12 text-center">
-          <Link
-            to="/profile/edit"
-            className="btn btn-default"
-          >
-            Edit Profile
-          </Link>
-        </div>
-      </div>
+      <Switch>
+        <Route exact path='/profile' render={() => (
+          <div className='ProfilePage'>
+            <h3>Profile Details</h3>
+            <div className="data-ctnr">
+              <label className="data-left" htmlFor="username">Username:</label>
+              <p id="username" className="data-right">{this.props.user.username}</p>
+            </div>
+            <div className="data-ctnr">
+              <label className="data-left" htmlFor="firstName">First Name (optional):</label>
+              <p id="firstName" className="data-right">{this.props.user.firstName}</p>
+            </div>
+            <div className="data-ctnr">
+              <label className="data-left" htmlFor="lastInitial">Last Initial (optional):</label>
+              <p id="lastInitial" className="data-right">{this.props.user.lastInitial}</p>
+            </div>
+            <div className="col-sm-12 text-center">
+              <Link
+                to="/profile/edit"
+                className="btn btn-default edit-btn"
+              >
+                Edit Profile
+              </Link>
+            </div>
+          </div>
+        )}/>
+        <Route exact path='/profile/edit' render={() => (
+          <div className='ProfilePage'>
+
+          </div>
+        )}/>
+      </Switch>
     );
   }
 }
