f5ae75656059366922a7adaa0d7b9fa7733ebb3b
Greg <gmerrill03@gmail.com>
2019-10-27 13:34:06 -0700

    Migrate protest info cards to own component modules

---
+++ src/components/ProtestCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import './css/ProtestCard.css';
+
+const ProtestCard = props => (
+  <Link
+    to={`/protests/${props.protest._id}`}
+    key={props.idx}
+    className="ProtestCard"
+  >
+    <div className="protest-details-ctnr">
+      <h3 className='protest-name'>{props.protest.name}</h3>
+      <p>{moment(props.protest.date).format("MMMM Do, YYYY")}</p>
+      <p>City: {props.protest.city}</p>
+      <p># stories: {props.protest.stories.length}</p>
+      <p>Location: {props.protest.location}</p>
+    </div>
+  </Link>
+)
+
+export default ProtestCard;
--- src/pages/ProtestsIndexPage.jsx
+++ src/pages/ProtestsIndexPage.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import moment from 'moment';
-import './css/ProtestsIndexPage.css'
+import './css/ProtestsIndexPage.css';
+import ProtestCard from '../components/ProtestCard';
 
 const ProtestsIndexPage = props => (
   <div className="ProtestsIndexPage">
     {props.protests.map((p, idx) => (
-      <Link
-        to={`/protests/${p._id}`}
-        key={idx}
-        className="protest-link"
-      >
-        <div className="protest-details-ctnr">
-          <h3 className='protest-name'>{p.name}</h3>
-          <p>{moment(p.date).format("MMMM Do, YYYY")}</p>
-          <p>City: {p.city}</p>
-          <p># stories: {p.stories.length}</p>
-          <p>Location: {p.location}</p>
-        </div>
-      </Link>
+      <ProtestCard
+        protest={p}
+        idx={idx}
+      />
     ))}
   </div>
 );
