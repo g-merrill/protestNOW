64669a379ed8f7a225fa50e1fb276528bcbb981c
Greg <gmerrill03@gmail.com>
2019-12-23 08:42:17 -0800

    Fix mongoose deprecation warning, remove redundant db file, add test folders

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const DB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/protestNOW';
 
-mongoose.connect(DB_URL, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true})
+mongoose.connect(DB_URL, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => console.log('MongoDB connected...'))
   .catch((err) => console.log(err));
 
